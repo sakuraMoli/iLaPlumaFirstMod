7cde9031725e45225ffb4693e4d4ca1e4e1a00e1
LaPluma <ArthurLee@wust.edu.cn>
2025-11-07 16:06:54 +0800

    first upload

---
+++ src/main/java/com/example/test/La_pluma_modDataGenerator.java
@@ -0,0 +1,11 @@
+package com.example.test;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class La_pluma_modDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
